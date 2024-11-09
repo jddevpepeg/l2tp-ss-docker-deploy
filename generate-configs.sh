a26f40fdc9d004b270a1e98dcaae3a26a68011df
jddevpepeg <125263690+jddevpepeg@users.noreply.github.com>
2024-11-09 23:24:17 +0800

    Update generate-configs.sh

--- generate-configs.sh
+++ generate-configs.sh
@@ -2,23 +2,6 @@
 
 IP_ADDRESS=`curl api.ip.sb`
 
-
-echo -n "Enter L2TP Username(blank to generate random string): "; read USERNAME
-echo -n "Enter L2TP Password(blank to generate random string): "; read PASSWORD
-echo -n "Enter L2TP PSK(blank to generate random string): "; read PSK
-
-if [[ ${USERNAME} == '' ]]; then USERNAME=`echo ${RANDOM} | md5sum | head -c 12`; fi
-if [[ ${PSK} == '' ]]; then PSK=`echo ${RANDOM} | md5sum | head -c 12`; fi
-if [[ ${PASSWORD} == '' ]]; then PASSWORD=`echo ${RANDOM} | md5sum | head -c 12`; fi
-
-cat << EOF > /opt/vpn.env
-VPN_USER=${USERNAME}
-VPN_PASSWORD=${PASSWORD}
-VPN_IPSEC_PSK=${PSK}
-VPN_DNS_SRV1=8.8.8.8
-VPN_DNS_SRV2=8.8.4.4
-EOF
-
 SS_PASSWORD=`tr -dc 'A-Za-z0-9!@$%#^' </dev/urandom | head -c 13`
 
 cat << EOF > /opt/shadowsocks-config.json
@@ -48,25 +31,7 @@ echo "${SS_URL}"
 
 cat << EOF > /root/vpn_config.txt
 ================================================
-
-IPsec VPN server is now ready for use!
-
-Connect to your new VPN with these details:
-
-Server IP: ${IP_ADDRESS}
-Username: ${USERNAME}
-Password: ${PASSWORD}
-IPsec PSK: ${PSK}
 SS URL: ${SS_URL}
-
-A more chinese friendly version:
-
-L2TP_VPN
-IP地址: ${IP_ADDRESS}
-用户: ${USERNAME}
-密码: ${PASSWORD}
-密钥: ${PSK}
-SS 链接：${SS_URL}
 ================================================
 EOF
 cat /root/vpn_config.txt
